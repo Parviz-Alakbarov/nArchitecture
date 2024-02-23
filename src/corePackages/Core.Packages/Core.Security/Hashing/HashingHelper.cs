467401bd9bd36a059573c147372d59fb0c72b61c
Parviz-Alakbarov <87971311+Parviz-Alakbarov@users.noreply.github.com>
2024-02-23 12:33:38 +0400

    Encryption & Hashing helpers added

---
+++ src/corePackages/Core.Packages/Core.Security/Hashing/HashingHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Core.Security.Hashing;
+
+public static class HashingHelper
+{
+    public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using HMACSHA512 hmac = new();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+    }
+
+    public static bool VerifyPasswordHash(string password, byte[] passwordSalt, byte[] passwordHash)
+    {
+        using HMACSHA512 hmac = new(passwordSalt);
+        byte[] computedHash = hmac.ComputeHash(passwordSalt);
+        return computedHash.SequenceEqual(passwordHash);
+    }
+}
